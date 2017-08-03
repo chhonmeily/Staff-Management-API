2a8e3e6a4047e993c35832e0e985d4df44b08968
Meily Chhon <chhonmeily@gmail.com>
2017-08-03 23:17:48 +0700

    learned about query language and mess around with controller

--- api/controllers/StaffController.js
+++ api/controllers/StaffController.js
@@ -1,11 +1,41 @@
 /**
- * UserController
+ * StaffController
  *
- * @description :: Server-side logic for managing users
+ * @description :: Server-side logic for managing staffs
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
 module.exports = {
-	
+
+	all: function(req, res){
+        Staff.find({
+            limit: 3
+        }).exec(function afterFind(err, staffs) {
+            if (err) {
+                // uh oh
+                // (handle error; e.g. `return res.serverError(err)`)
+                return res.serverError(err);
+            }
+        
+            return res.json(staffs)
+        });    
+    },
+
+    perpage: function(req, res){
+        
+        var numOfStaffPerPage = req.params.perpage;
+
+        Staff.find({
+            skip:0,
+            limit: numOfStaffPerPage,
+            sort: 'createAt DESC'
+        }).exec(function (err, staffs){
+            if (err) {
+                return res.serverError(err);
+            }
+
+            return res.json(200, staffs);
+        });
+    }
 };
 
